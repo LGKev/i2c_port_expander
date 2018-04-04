6514d5165550cbfc6f23a88b857b0e9f43b6686c
U-SPARKFUN\kevin.kuwata <kevin.kuwata@SX-KEVIN.sparkfun.local>
2018-04-04 12:15:25 -0600

    initial code

---
+++ Qwiic_GPIO_Hello.ino
@@ -0,0 +1,77 @@
+/*
+  This is the first attempt at communicating with Qwiic Gpio.
+
+  Created May, 4th 2018
+
+  Author: Kevin Kuwata @SparkX
+*/
+
+#include <Wire.h>
+
+byte const qwiicGpioAddress = 0x27; //When all jumpers are closed A0, A1, A2 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Start");
+  Wire.begin();
+
+  testForConnectivity();
+
+  //Attach interrupts
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+// testForConnectivity() checks for an ACK from an Relay. If no ACK
+// program freezes and notifies user.
+void testForConnectivity() {
+  Wire.beginTransmission(qwiicGpioAddress);
+  //check here for an ACK from the slave, if no ack don't allow change?
+  if (Wire.endTransmission() != 0) {
+    Serial.println("Check Connections. No slave attached.");
+    while (1);
+  }
+}
+
+/*
+  Send data to slave
+    - Start and address
+    - Send data
+    - Stop conddition
+
+  Read data from slave
+    - Start and address
+    - send requested register
+    - mast rx (this would be the request # of bytes)..
+        - like the getFirmwareVersion() function below
+
+
+  //getFirmwareVersion() returns the firmware version as a float.
+  float getFirmwareVersion() {
+  Wire.beginTransmission(address);
+  Wire.write(__REGISTER___);
+  Wire.endTransmission();
+
+  Wire.requestFrom(qwiicRelayAddress, 2); //2 BYTES for the Version Number
+
+//collect response.
+  if (Wire.available() == 0) return 0;
+  float versionNumber = Wire.read();
+  versionNumber += (float)Wire.read() / 10.0;
+
+  return (versionNumber);
+  }
+
+
+
+
+*/
+
