41053609e5620962039f47de983998fc9a707da5
kevin Kuwata <kevin.kuwata@SX-KEVIN.sparkfun.local>
2018-04-12 14:01:33 -0600

    adding more details

--- Qwiic_GPIO_Hello.ino
+++ Qwiic_GPIO_Hello.ino
@@ -262,7 +262,8 @@ void loop(){
 
 	setPinOutput(qwiicGpioAddress, 8, HIGH);
 	delay(250);
-	setPinOutput(qwiicGpioAddress, 8, LOW);
+	setPinOutput(qwiicGpioAddress, 8, LOW); //results in pin 4 turning on off
+	//similarly sendin 5 will make pins 1 and 3 turn on off bc 0b0101 is 5 in binary.
 	delay(250);
 	
 	
